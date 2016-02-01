83e43232d5048de7499dc10193330ea5d552e47f
Ryan Tanay <ryantanay@newevolution.org>
2016-02-01 11:14:04 -0600

    Added a head write, enabled setting the port number via command line argument

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 "use strict";
 
 const http = require("http");
-const port = 3000;
+const {PORT} = process.env;
 
 http.createServer((req, res) => {
   console.log(req.method, req.url);
-
-  res.end("Done");
-}).listen(port, () => {
-         console.log("Node.js server started. lisetening on port", port);
+  res.writeHead(200, {
+               "Content-type": "text/html"
+  });
+  res.end("&#23646;&#34278;");
+}).listen(PORT, () => {
+         console.log(`Node.js server started. lisetening on port ${PORT}`);
 });
