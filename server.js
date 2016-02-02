72313b6fb3df42011f0fc2290f1b3ebbe1faf399
Ryan Tanay <ryantanay@newevolution.org>
2016-02-02 09:15:35 -0600

    Provided default port value if one is not provided as a CLI argument, removed debugger demo code

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 "use strict";
 
 const http = require("http");
-const {PORT} = process.env;
+const PORT = process.env.PORT || 3000;
 
 http.createServer((req, res) => {
   console.log(req.method, req.url);
   res.writeHead(200, {
                "Content-type": "text/html"
   });
-  debugger;
+
   res.end("&#23646;&#34278;");
 }).listen(PORT, () => {
          console.log(`Node.js server started. lisetening on port ${PORT}`);
