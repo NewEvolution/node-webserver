aadb679d000cc8786fddc873f1f95c8a728db96a
Ryan Tanay <ryantanay@newevolution.org>
2016-02-02 10:47:07 -0600

    Added cases for various URLs, messed around with a contrived case for using .write with a bunch of timeouts, added fixes for favicon.ico so it would have one and stop requesting it constantly

--- server.js
+++ server.js
@@ -1,15 +1,38 @@
 "use strict";
 
+const fs = require("fs");
 const http = require("http");
 const PORT = process.env.PORT || 3000;
 
 http.createServer((req, res) => {
   console.log(req.method, req.url);
-  res.writeHead(200, {
-               "Content-type": "text/html"
-  });
-
-  res.end("&#23646;&#34278;");
+  switch(req.url) {
+    case "/favicon.ico":
+      res.writeHead(200, {'Content-Type': 'image/x-icon'} );
+      const img = fs.readFileSync('./favicon.ico');
+      res.end(img, 'binary');
+      console.log('favicon requested');
+      break;
+    case "/hello":
+      res.writeHead(200, {"Content-Type": "text/html"});
+      const msg = "<h1>Hello world</h1>";
+      msg.split("").forEach((letter, i) => {
+        setTimeout(() => {
+          res.write(letter);
+        }, 100 * i);
+      });
+      setTimeout(() => {
+        res.end("<h3>Goodbye world...</h3>");
+      }, 2500);
+      break;
+    case "/random":
+      res.end(Math.random().toString());
+      break;
+    default:
+      res.writeHead(403);
+      res.end("Access denied!");
+      break;
+  }
 }).listen(PORT, () => {
          console.log(`Node.js server started. lisetening on port ${PORT}`);
 });
