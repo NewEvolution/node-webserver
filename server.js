c720158360eb6340453d0dc9fc5c19ed7d0e01a3
Ryan Tanay <ryantanay@newevolution.org>
2016-02-02 11:10:52 -0600

    Refactored server to use express

--- server.js
+++ server.js
@@ -1,38 +1,38 @@
 "use strict";
 
 const fs = require("fs");
-const http = require("http");
+const app = require("express")();
 const PORT = process.env.PORT || 3000;
 
-http.createServer((req, res) => {
-  console.log(req.method, req.url);
-  switch(req.url) {
-    case "/favicon.ico":
-      res.writeHead(200, {'Content-Type': 'image/x-icon'} );
-      const img = fs.readFileSync('./favicon.ico');
-      res.end(img, 'binary');
-      console.log('favicon requested');
-      break;
-    case "/hello":
-      res.writeHead(200, {"Content-Type": "text/html"});
-      const msg = "<h1>Hello world</h1>";
-      msg.split("").forEach((letter, i) => {
-        setTimeout(() => {
-          res.write(letter);
-        }, 100 * i);
-      });
-      setTimeout(() => {
-        res.end("<h3>Goodbye world...</h3>");
-      }, 2500);
-      break;
-    case "/random":
-      res.end(Math.random().toString());
-      break;
-    default:
-      res.writeHead(403);
-      res.end("Access denied!");
-      break;
-  }
-}).listen(PORT, () => {
-         console.log(`Node.js server started. lisetening on port ${PORT}`);
+app.get("/favicon.ico", (req, res) => {
+  res.writeHead(200, {'Content-Type': 'image/x-icon'} );
+  const img = fs.readFileSync('./favicon.ico');
+  res.end(img, 'binary');
+  console.log('favicon requested');
+});
+
+app.get("/hello", (req, res) => {
+  res.writeHead(200, {"Content-Type": "text/html"});
+  const msg = "<h1>Hello world</h1>";
+  msg.split("").forEach((letter, i) => {
+    setTimeout(() => {
+      res.write(letter);
+    }, 100 * i);
+  });
+  setTimeout(() => {
+    res.end("<h3>Goodbye world...</h3>");
+  }, 2500);
+});
+
+app.get("/random", (req, res) => {
+  res.end(Math.random().toString());
+});
+
+app.get("*", (req, res) => {
+  res.writeHead(403);
+  res.end("Access denied!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Node.js server started. lisetening on port ${PORT}`);
 });
