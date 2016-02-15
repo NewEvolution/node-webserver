bbac2e3d67bb3c62d1bddafc6069bceba1090755
Ryan Tanay <ryantanay@newevolution.org>
2016-02-15 14:27:10 -0600

    Modified remote DB access with ternary

--- server.js
+++ server.js
@@ -10,9 +10,14 @@ const routes = require('./routes/')
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-const MONGODB_AUTH = process.env.MONGODB_AUTH || '';
+const MONGODB_USER = process.env.MONGODB_USER || '';
+const MONGODB_PASS = process.env.MONGODB_PASS || '';
 const MONGODB_HOST = process.env.MONGODB_HOST || 'localhost';
 const MONGODB_PORT = process.env.MONGODB_PORT || 27017;
+
+const MONGODB_AUTH = MONGODB_USER
+  ? `${MONGODB_USER}:${MONGODB_PASS}@`
+  : '';
 
 const MONGODB_URL = `mongodb://${MONGODB_AUTH}${MONGODB_HOST}:${MONGODB_PORT}/node-webserver`;
 
