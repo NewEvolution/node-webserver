2ebc5f2ddb1f3d21d50e1a42262ae41743d2e8b9
Ryan Tanay <ryantanay@newevolution.org>
2016-09-12 16:19:29 -0500

    Fixed weird longtime buggy behavior in web caledar display with the judicious application of parseInt

--- controllers/cal.js
+++ controllers/cal.js
@@ -21,15 +21,15 @@ function successHeader (res, type) {
 // GET /cal
 module.exports.month = (req, res) => {
   successHeader(res, 'text/html');
-  const month = req.params.month - 1;
-  const year = req.params.year;
+  const month = parseInt(req.params.month - 1);
+  const year = parseInt(req.params.year);
   const monthArr = utility.buildMonth(month, year);
   printCal(res, monthArr);
 };
 
 module.exports.year = (req, res) => {
   successHeader(res, 'text/html');
-  const year = req.params.year;
+  const year = parseInt(req.params.year);
   const yearArr = utility.buildYear(year);
   printCal(res, yearArr);
 };
