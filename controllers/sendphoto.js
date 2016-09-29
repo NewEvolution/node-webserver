5c9ed247c93bf1857b8f884fbbbd7d3e9c4f3ed7
Ryan Tanay <ryantanay@newevolution.org>
2016-09-29 14:27:05 -0500

    Modified image submission, less pretty, but functional now

--- controllers/sendphoto.js
+++ controllers/sendphoto.js
@@ -9,12 +9,13 @@ module.exports.index = (req, res) => {
 };
 
 module.exports.new = (req, res) => {
+  res.write('<h1>Thanks for your image!</h1>');
   const extension = path.extname(req.file.originalname);
   const tempPath = req.file.path;
   const newPath = tempPath + extension;
   fs.rename(tempPath, newPath, err => {
     if (err) {
-      res.send(`<p>Something has gone wrong: <strong>${err.message}</strong></p>`);
+      res.end(`<p>Something has gone wrong: <strong>${err.message}</strong></p>`);
       throw err;
     }
     imgur.uploadFile(newPath)
@@ -25,11 +26,10 @@ module.exports.new = (req, res) => {
       res.end(`<a href='${pageUrl}'><img src='${pageUrl}l${extension}' alt='Your image'></a>
               <p><a href='${pageUrl}l${extension}'>Large Thumbnail</a></p>
               <p><a href='${pageUrl}m${extension}'>Medium Thumbnail</a></p>
-              <p><a ef='${pageUrl}s${extension}'>Small Thumbnail</a></p>`);
+              <p><a href='${pageUrl}s${extension}'>Small Thumbnail</a></p>`);
     }).catch(err => {
       fs.unlink(newPath);
       res.end(`<p>Something has gone wrong: <strong>${err.message}</strong></p>`);
     });
   });
-  res.write('<h1>Thanks for your image!</h1>');
 };
